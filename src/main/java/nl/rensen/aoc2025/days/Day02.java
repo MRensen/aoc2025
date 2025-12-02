630dd72027c9274cdeb5313684e8fe431a45b284
MRensen <markrensen@hotmail.com>
2025-12-02 22:01:05 +0100

    part 2 cleanup and citation

--- src/main/java/nl/rensen/aoc2025/days/Day02.java
+++ src/main/java/nl/rensen/aoc2025/days/Day02.java
@@ -46,17 +46,12 @@ public class Day02 implements Day<Long, String> {
                 if(isSequence(string_value)){
                     results.add(string_value);
                 }
-//                String second_half = string_value.substring(string_value.length()/2);
-//                if(string_value.length()%2==0 && string_value.startsWith(second_half)){
-//                    results.add(string_value);
-//                }
 
             });
         }
 
         Long result = 0L;
         for(String id : results){
-//            System.out.println(id);
             result += Long.parseLong(id);
         }
         return result;
@@ -65,11 +60,10 @@ public class Day02 implements Day<Long, String> {
         if(id.length()<2){
             return false;
         }
-        String first = id.substring(0,1);
-        String rest =id.substring(1);
         StringBuilder acc = new StringBuilder();
             for(char c : id.toCharArray()) {
                 acc.append(c);
+//                https://www.baeldung.com/java-string-count-occurences-of-sequence
                 int count = Math.toIntExact(Pattern.compile(Pattern.quote(acc.toString()))
                         .matcher(id)
                         .results()
@@ -81,31 +75,6 @@ public class Day02 implements Day<Long, String> {
                     }
                 }
             }
-//        while(!rest.isEmpty()){
-//            StringBuilder acc = new StringBuilder();
-//            for(char c : rest.toCharArray()){
-//                acc.append(c);
-//                if(acc.toString().equals(first) && rest.isEmpty()){
-//                    return true;
-//                }else {
-//
-//                }
-//
-//            }
-
-//            if(first.startsWith(rest.substring(0,1))){
-//                if(rest.length()>= first.length()) {
-//                    String checkPart = rest.substring(0, first.length());
-//                    if (checkPart.equals(first)) {
-//                        return true;
-//                    }
-//                }
-//
-//            } else{
-//                first = first+rest.substring(0,1);
-//                rest = rest.substring(1);
-//            }
-
         return false;
     }
 }
