6e7237b0fb8931e9fc99af8e44b7350d7dcb057c
MRensen <markrensen@hotmail.com>
2025-12-15 21:36:20 +0100

    day 8 part 2

--- src/main/java/nl/rensen/aoc2025/days/Day08.java
+++ src/main/java/nl/rensen/aoc2025/days/Day08.java
@@ -158,7 +158,8 @@ public class Day08 implements Day<Integer, String> {
             }
         }
 
-
+//        Sorteer alle gevonden circuits met compareTo (vergelijkt de members.size van twee circuits)
+//        Daarna vermenigvuldigt dit stukje de sizes van de grootste 3 circuits met elkaar.
         circuits.sort(Circuit::compareTo);
         int count = 1;
         for(int i = 0; i < 3; i++){
@@ -169,5 +170,91 @@ public class Day08 implements Day<Integer, String> {
 
     @Override
     public Integer part2(List<String> input) {
-    return 0;}
+
+        List<Cord> cords = new ArrayList<>();
+        for(String str : input) {
+            String[] inputs = str.split(",");
+            cords.add(new Cord(Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2])));
+        }
+
+        List<DistancePair> pairs = new ArrayList<>();
+        for(int i = 0; i < cords.size(); i++){
+            Cord base =  cords.get(i);
+            for(int j = i+1; j < cords.size(); j++){
+                Cord compare = cords.get(j);
+                var distance =  // https://www.geeksforgeeks.org/maths/euclidean-distance/
+                        Math.pow(base.x-compare.x, 2) +
+                                Math.pow(base.y - compare.y, 2) +
+                                Math.pow(base.z - compare.z, 2)
+                        ;
+                pairs.add(new DistancePair(base, compare, distance));
+            }
+        }
+        pairs.sort(DistancePair::compareTo);
+
+//        Dit is niet meer nodig voor part 2, nu wordt namelijk de volledige lijst gebruikt totdat het resultaat bereikt is.
+//        pairs = pairs.subList(0, SAMPLE_SIZE);
+
+        List<Circuit> circuits = new ArrayList<>();
+        for(int i = 0; i < pairs.size(); i++){
+            DistancePair pair = pairs.get(i);
+            if(circuits.isEmpty()){
+                Circuit circuit = new Circuit();
+                circuit.add(pair.cord1);
+                circuit.add(pair.cord2);
+                pair.cord1.circuit = circuit;
+                pair.cord2.circuit = circuit;
+                circuits.add(circuit);
+                continue;
+            }
+            boolean circuitFound = false;
+            boolean coupleCircuit = false;
+            Circuit foundCircuit = null;
+            Circuit foundCoupleCircuit = null;
+
+            for(Circuit circuit : circuits){
+                if(circuit.contains(pair.cord1) || circuit.contains(pair.cord2)){
+                    if(circuitFound){
+                        coupleCircuit = true;
+                        foundCoupleCircuit = circuit;
+                    } else {
+                        circuitFound = true;
+                        foundCircuit = circuit;
+                    }
+                }
+            }
+            if(coupleCircuit){
+                for(Cord cord : foundCoupleCircuit.members){
+                    foundCircuit.add(cord);
+                }
+                circuits.remove(foundCoupleCircuit);
+            }
+            else if(!circuitFound){
+                Circuit circuit = new Circuit();
+                circuit.add(pair.cord1);
+                circuit.add(pair.cord2);
+                circuits.add(circuit);
+                continue;
+            } else {
+                foundCircuit.add(pair.cord1);
+                foundCircuit.add(pair.cord2);
+            }
+
+//            Alles wat nodig is voor part 2, is deze extra check.
+//            Wanneer er slechts 1 circuit is en de members-lijst van deze circuit alle coordinaten bevat (even lang als de coordinaten lijst), return dan het resultaat.
+            if(circuits.size() == 1 && cords.size() == circuits.get(0).members.size()){
+                return pair.cord1.x * pair.cord2.x;
+            }
+        }
+
+//      Deze volgende berekeningen zijn niet meer nodig voor part 2
+
+//        circuits.sort(Circuit::compareTo);
+//        int count = 1;
+//        for(int i = 0; i < 3; i++){
+//            count *= circuits.get(i).size();
+//        }
+//        return count;
+        return 0;
+    }
 }
--- src/test/java/nl/rensen/aoc2025/days/Day08Test.java
+++ src/test/java/nl/rensen/aoc2025/days/Day08Test.java
@@ -11,19 +11,17 @@ public class Day08Test {
 
 
     @Rule
-    public DayInputExternalResource input = new DayInputExternalResource(99);
+    public DayInputExternalResource input = new DayInputExternalResource(8);
 
     @Test
     public void part1() {
         Day<Integer, String> day = new Day08();
         assertEquals(Integer.valueOf(83520), day.part1(input.getLines()));
-//        2033 -> too high
     }
 
     @Test
     public void part2() {
         Day<Integer, String> day = new Day08();
-        assertEquals(Integer.valueOf(0), day.part2(input.getLines()));
-//        164 too low
+        assertEquals(Integer.valueOf(1131823407), day.part2(input.getLines()));
     }
 }
